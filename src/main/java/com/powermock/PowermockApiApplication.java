f6023ec8765a9663c96b50d16cf91514072ab441
محمد ايمن شراده <mcharrada@tvtc.gov.sa>
2019-10-23 11:37:48 +0300

    First commit of project

---
+++ src/main/java/com/powermock/PowermockApiApplication.java
@@ -0,0 +1,13 @@
+package com.powermock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowermockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowermockApiApplication.class, args);
+	}
+
+}
